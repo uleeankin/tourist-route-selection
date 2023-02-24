e24dc95380168b23c490cdf02a8a00b974ccb24f
uleeankin <sokolowa.yu.a@gmail.com>
2023-02-24 12:57:24 +0300

    init project

---
+++ src/main/java/com/example/touristrouteselection/TouristRouteSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristrouteselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristRouteSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristRouteSelectionApplication.class, args);
+    }
+
+}
